93f9ff296d912a465c60c30e8fe22fb82f91a4b7
AlexanderBS <alex.sad@bk.ru>
2018-03-05 23:03:58 +0800

    make homework#3 Collection framework

--- Java1/src/main/java/javaCore/lesson3/Main.java
+++ Java1/src/main/java/javaCore/lesson3/Main.java
@@ -1,9 +1,42 @@
 package javaCore.lesson3;
 
+import java.util.*;
+
 /**
- * Created by AlexBS on 04.03.2018.
+ * Created by AlexBS on 05.03.2018.
  */
 public class Main {
     public static void main(String[] args) {
+        String text = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum quas, beatae nihil asperiores " +
+                "rem ipsum, placeat ab earum nulla omnis fugit assumenda labore Lorem ipsum dolor sit amet.";
+        task(text);
+        phoneBook();
+    }
+
+
+    private static void task(String str) {
+        ArrayList<String> data = new ArrayList<String>();
+        Integer value;
+        String[] words = str.split("[ ,.]+");
+        Map<String, Integer> lhm = new LinkedHashMap<>();
+        for (String word : words) {
+            value = lhm.get(word);
+            lhm.put(word, ((value == null)? 1 : value + 1));
+        }
+        for (String word : words) {
+            if (lhm.get(word) == 1) {
+                data.add(word);
+            }
+        }
+        System.out.println("counted words: \n" + lhm);
+        System.out.println("list of unique words:\n" + data);
+        }
+
+    private static void phoneBook() {
+        PhoneBook pb = new PhoneBook();
+        pb.add("Ivanov","12345");
+        pb.add("Sidorov","123456");
+        pb.add("Ivanov","123456789");
+        System.out.println(pb.get("Ivanov"));
     }
 }
