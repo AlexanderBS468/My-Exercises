25f1a9ffef7be1a2ee58ecf9a9056b3c5bfb33b3
AlexanderBS <alex.sad@bk.ru>
2018-03-07 15:29:25 +0800

    #remove on folder lesson1

--- Java1/src/main/java/javaCore/obstacles/Obstacle.java
+++
@@ -1,9 +0,0 @@
-package javaCore.obstacles;
-
-import javaCore.participants.Participant;
-/**
- * Created by AlexBS on 26.02.2018.
- */
-public abstract class Obstacle {
-    public abstract void doIt(Participant p);
-}
