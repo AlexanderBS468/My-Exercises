a0116ae1688af5237d8b0b3c7396ad5b96aa428a
AlexanderBS <alex.sad@bk.ru>
2018-03-07 15:28:46 +0800

    #remove on dir lesson1

---
+++ Java1/src/main/java/javaCore/lesson1/obstacles/Obstacle.java
@@ -0,0 +1,9 @@
+package javaCore.lesson1.obstacles;
+
+import javaCore.lesson1.participants.Participant;
+/**
+ * Created by AlexBS on 26.02.2018.
+ */
+public abstract class Obstacle {
+    public abstract void doIt(Participant p);
+}
